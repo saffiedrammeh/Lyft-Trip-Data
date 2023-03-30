3f8b770ea3cef81470c5e1089055c53feb614898
saffiedrammeh <129075648+saffiedrammeh@users.noreply.github.com>
2023-03-30 16:08:30 -0400

    Add files via upload

---
+++ SQLite.3.sql
@@ -0,0 +1,44 @@
+SELECT * FROM trips;
+
+SELECT * FROM riders;
+
+SELECT * FROM cars;*/
+
+
+
+SELECT *
+FROM trips AS T
+LEFT JOIN riders AS R
+ON T.rider_id = R.id;
+
+SELECT *
+FROM riders AS R
+LEFT JOIN trips AS T
+ON R.id = T.rider_id;
+
+SELECT * 
+FROM trips
+JOIN cars
+ON trips.car_id = cars.id;
+
+SELECT *
+FROM riders
+UNION
+SELECT *
+FROM riders2;
+
+SELECT AVG(cost)
+FROM trips;
+
+SELECT *
+FROM riders
+WHERE total_trips < 500;
+
+SELECT COUNT (*)
+FROM cars
+WHERE status = 'active';
+
+SELECT *
+FROM cars
+ORDER BY trips_completed DESC
+LIMIT 2;
